5078cc18175b0cd994a746c6fceb780cdbb57127
Sergey Danilkin <danilkin.sergey@gmail.com>
2017-08-17 18:31:09 +0300

    add reservation

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -13,6 +13,16 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Landon', $crawler->filter('h4')->text());
     }
+    public function testAbout()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/about');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Aboutk', $crawler->filter('h4')->text());
+
+    }
+
 }
